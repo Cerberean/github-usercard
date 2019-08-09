cbe146dc8cec4dd3a613b32dc8ccbd651bf4ecf8
Christopher Hernandez <cerberean661@gmail.com>
2019-08-08 18:45:45 -0700

    finishing up

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,29 @@
            https://api.github.com/users/<your name>
 */
 
+function axiosFunction(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      // console.log(response);
+      profileBox(response.data);
+      return username;
+    })
+    // .then(username => {
+    //   axios.get(`https://api.github.com/users/${username}/followers`)
+    //     .then(response => {
+    //       const followerCount = response.data;
+    //       followerCount.forEach(item => {
+    //         axios.get(`https://api.github.com/users/${item.login}`)
+    //           .then(response => {
+    //             profileBox(response.data);
+    //           })
+    //       })
+    //     })
+    // })
+
+}
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +47,20 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersList = [
+  "Krishan-Nattar",
+  "shanreed",
+  "raajnpatel",
+  "michelangeloxo"
+];
+
+followersList.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+    .then(response => {
+      // console.log(response);
+      profileBox(response.data);
+    })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -43,7 +79,6 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
 /* List of LS Instructors Github username's: 
@@ -53,3 +88,72 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const pagePost = document.querySelector(".cards");
+
+function profileBox(object) {
+
+  // console.log(object);
+
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+
+  const avatarImg = document.createElement("img");
+  avatarImg.src = object.avatar_url;
+
+  const cardInfoDiv = document.createElement("div");
+  cardInfoDiv.classList.add("card-info");
+
+  const nameH3 = document.createElement("h3");
+  nameH3.classList.add("name");
+  nameH3.textContent = object.name;
+
+  const usernameP = document.createElement("p");
+  usernameP.classList.add("username");
+  usernameP.textContent = object.login;
+
+  const locationP = document.createElement("p");
+  locationP.textContent = `Location: ${object.location}`;
+
+  const profileP = document.createElement("p");
+  profileP.textContent = `Profile:`;
+
+  const profileAnchor = document.createElement("a");
+  profileAnchor.href = object.html_url;
+  profileAnchor.textContent = 'Github Page';
+
+  profileP.appendChild(profileAnchor);
+
+  const followersP = document.createElement("p");
+  followersP.textContent = object.followers;
+
+  const followingP = document.createElement("p");
+  followingP.textContent = object.following;
+
+  const bioP = document.createElement("p");
+  bioP.textContent = `Bio: ${object.bio}`;
+
+  const chartImg = document.createElement("img");
+  chartImg.src = `http://ghchart.rshah.org/${object.login}`;
+  chartImg.style.width = "100%";
+
+  
+
+  cardInfoDiv.appendChild(nameH3);
+  cardInfoDiv.appendChild(usernameP);
+  cardInfoDiv.appendChild(locationP);
+  cardInfoDiv.appendChild(profileP);
+  cardInfoDiv.appendChild(followersP);
+  cardInfoDiv.appendChild(followingP);
+  cardInfoDiv.appendChild(bioP);
+  cardInfoDiv.appendChild(chartImg);
+
+  cardDiv.appendChild(avatarImg);
+  cardDiv.appendChild(cardInfoDiv);
+
+  pagePost.appendChild(cardDiv);
+
+  console.log(cardDiv)
+}
+
+axiosFunction("Cerberean");
